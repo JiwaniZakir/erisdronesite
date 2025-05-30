5e037439b6d2074d4cd092caef519595d765b823
Your Name <you@example.com>
2025-05-30 12:11:48 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import '../framer/styles.css'
+// Import your Framer components here, for example:
+// import Menu from '../framer/menus'
+
+function App() {
+  return (
+    <div className='flex flex-col items-center gap-3 '>
+      {/* Add your Framer components here, for example: */}
+      {/* <Menu componentVariable='some variable' /> */}
+    </div>
+  );
+}
+
+export default App
